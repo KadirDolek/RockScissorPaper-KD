8b7b17df8f732f8bde676f31553f1e8b42a1fce6
kadirdolek <kadirdolek@outlook.com>
2025-05-15 13:50:16 +0200

    Css content done

--- src/Components/Content/Content.jsx
+++ src/Components/Content/Content.jsx
@@ -1,22 +1,53 @@
+import React,{ useState } from 'react';
 import './Content.css'
 
 
-export default function Content(){
+export default function Content() {
+    
+    const choixRandom =['paper', 'rock','scissor']
+    const [monChoix, setMonChoix] = useState('');
+    const [choixOrdi, setChoixOrdi] = useState('');
 
 
+    const handleClick = (e) => {
+        const monClick = e.target.id;
+        choixAleatoire = choixRandom[Math.floor(Math.random() * choixRandom.length)];
 
-    return(
+        setMonChoix =(monClick)
+        setChoixOrdi(choixAleatoire)
+        
+    };
+
+    return (
         <div id='content1'>
-            <div id='divPaper' style={{width:'300px', height:'250px'}}>
-                <img id='paper' src="../src/assets/images/icon-paper.svg" alt="" />
+            <div id='divPaper' style={{ width: '300px', height: '250px' }}>
+                <img
+                    style={{ cursor: 'pointer' }}
+                    onClick={handleClick}
+                    id='paper'
+                    src="../src/assets/images/icon-paper.svg"
+                    alt="papier"
+                />
             </div>
-            <div id='divScissor' style={{width:'300px', height:'250px'}}>
-                <img id='scissor' src="../src/assets/images/icon-scissors.svg" alt="" />
+            <div id='divScissor' style={{ width: '300px', height: '250px' }}>
+                <img
+                    style={{ cursor: 'pointer' }}
+                    onClick={handleClick}
+                    id='scissor'
+                    src="../src/assets/images/icon-scissors.svg"
+                    alt="ciseaux"
+                />
             </div>
-            <div id='divRock' style={{width:'250px', height:'250px'}}>
-                <img id='rock' src="../src/assets/images/icon-rock.svg" alt="" />
+            <div id='divRock' style={{ width: '250px', height: '250px' }}>
+                <img
+                    style={{ cursor: 'pointer' }}
+                    onClick={handleClick}
+                    id='rock'
+                    src="../src/assets/images/icon-rock.svg"
+                    alt="pierre"
+                />
             </div>
- 
+            
         </div>
-    )
+    );
 }
