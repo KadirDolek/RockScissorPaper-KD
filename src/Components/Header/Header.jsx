2ccfb4e7269d3314d95288c79d23d8743e8a1892
kadirdolek <kadirdolek@outlook.com>
2025-05-15 10:49:02 +0200

    travail sur header

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import './Header.css'
+
+export default function Header(){
+
+    
+
+    return(
+         <div id='header'>
+            <div id='header2'>
+                <p>ROCK</p>
+                <p>PAPER</p>
+                <p>SCISSORS</p>
+            </div>
+            <div id='header3'>
+                <p>SCORE</p>
+            </div>
+        </div>
+    )
+}
